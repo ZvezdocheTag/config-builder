994d5898a2461b7fc15cc2c4d934d95270eb8005
Yastremsky Vitaly <yastremsky@gambit.ru>
2017-04-10 22:36:54 +0300

    action for req

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,21 @@ export const REMOVE_RULE = "REMOVE_RULE"
 export const COUNT_CARD = "COUNT_CARD"
 export const SET_RULES_TAB = "SET_RULES_TAB"
 
+export const GET_DATA = "GET_DATA"
+export const RECIEVE_CARD = "RECIEVE_CARD"
+
+export const getData = (data) => ({
+    type: "GET_DATA",
+    data
+})
+
+export const recieveCard = (data, json) => {
+    return {
+        type: RECIEVE_CARD,
+        data,
+        card: json
+    }
+}
 
 export const addRule = (text) => ({
     type: "ADD_RULE",
